8d25747e5a0ce887d161c6196af00ce8f56bef9a
kanekii16 <iamsai1616@gmail.com>
2023-06-16 05:27:48 +0530

    Modd  Soln

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -24,7 +24,7 @@ public class F1 extends Car {
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
-            this.stop();
+            stop();
         }
         //for all other cases, change the gear accordingly
 
